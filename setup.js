d175332528773a145971db0e121a974f2b08651a
normandiegirl <129623806+normandiegirl@users.noreply.github.com>
2024-02-29 08:50:57 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 require('@babel/register');
 require('ts-node').register({ transpileOnly: true });
-
+// testing //
 require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
