6a9004a36bcdc8adfadceb1e224d4d0d9675803a
normandiegirl <129623806+normandiegirl@users.noreply.github.com>
2024-05-18 16:50:24 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,5 +1,5 @@
-import { getMessage } from '../../ui/helper/utils/i18n-helper';
-import * as en from '../../app/_locales/en/messages.json';
+import { getMessage } from '../ui/helper/utils/i18n-helper';
+import * as en from '../app/_locales/en/messages.json';
 
 export function tEn(keys) {
   return getMessage('en, keys);
