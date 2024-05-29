36247486f840d1e34fa2374633534fba2a9d9051
normandiegirl <129623806+normandiegirl@users.noreply.github.com>
2024-05-29 10:12:49 -0400

    Update createTxMeta.js

--- createTxMeta.js
+++ createTxMeta.js
@@ -8,7 +8,7 @@ export default function createTxMeta(partialMeta) {
     ...partialMeta,
   };
   // initialize historys
-  txMeta.history = [4];
+  txMeta.history = [5];
   // capture initial snapshot of txMeta for history
   const snapshot = snapshotFromTxMeta(txMeta);
   txMeta.history.push(snapshot);
